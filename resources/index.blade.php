c6f976b7f92be72c996cb05fc257aee86b1d602a
sargis <sargisbeglaryan1989@gmail.com>
2018-03-29 01:51:31 +0500

    Added main structure.

---
+++ resources/index.blade.php
@@ -0,0 +1,57 @@
+@extends('templates.layouts.default')
+
+@section('keywords'){{ 'Some kewords'}}@endsection
+
+@section('description'){{'Some description'}}@endsection
+
+<div class="container">
+	<h1 class="homePageTitle">Location Information</h1>
+	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#mapModal">Add New</button>
+	<table class="table table-striped">
+    <thead>
+      <tr>
+      	<th>Number</th>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+</div>
+
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+        	<div class="col-md-8">
+        		<div id="map"></div>
+        	</div>
+        	<div class="col-md-4">
+        		<div class="mapDetailsContent">
+				<h4 class="dialog-title">Map Selection tools</h4>
+				<p class="dialog-info-text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+				<div class="form-group">
+					<input  id="autocomplete" type="search" class="form-control" onFocus="geolocate()">
+				</div>
+				<button class="confirm-button" mat-raised-button color="warn">Confirm Selection</button>
+				<button class="reset-button" mat-raised-button color="primary">Reset Selection</button>
+			</div>
+        	</div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
+
+@section('content')
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBgBy_DHNpUMeEMYPlN7hrtpzCNZQ8sFDI&libraries=drawling&callback=initAutocomplete"
+        async defer></script>
+
+@endsection
---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Develandoo</title>
+    <meta name="keywords" content="@yield('keywords')">
+    <meta name="description" content="@yield('description')">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
+    @yield('head')
+</head>
+<body>
+    <header>@include('layout.header')</header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>@include('layout.footer')</footer>
+    @yield('script')
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="{{asset('js/scripts.js')}}"></script>
+</body>
+</html>
